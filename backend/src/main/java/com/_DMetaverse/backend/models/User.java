1cd0c21edc6d20c422a7f71bbf860138713a0c22
22051339 <himanshusaini0803@gmail.com>
2025-10-21 22:54:51 +0530

    starting project

---
+++ backend/src/main/java/com/_DMetaverse/backend/models/User.java
@@ -0,0 +1,68 @@
+package com._DMetaverse.backend.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Long id;
+
+    @Column(name="email", unique=true, nullable=false)
+    private String email;
+
+    @Column(name="password")
+    private String password;
+
+    @Column(name="avatar")
+    private Integer avatarId;
+
+    @Column(name="refresh_token")
+    private String refreshToken;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getAvatarId() {
+        return avatarId;
+    }
+
+    public void setAvatarId(int avatarId) {
+        this.avatarId = avatarId;
+    }
+
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+
+    public void setRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+
+    @Override
+    public String toString() {
+        return "User [id=" + id + ", password=" + password + ", avatarId=" + avatarId + ", refreshToken=" + refreshToken
+                + "]";
+    }
+}
