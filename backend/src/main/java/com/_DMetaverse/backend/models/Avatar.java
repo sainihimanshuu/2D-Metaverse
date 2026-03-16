f8ef3c78b5047b886e6271c83295eb0b53d29bde
22051339 <himanshusaini0803@gmail.com>
2026-03-16 15:32:18 +0530

    basic models for MVP

---
+++ backend/src/main/java/com/_DMetaverse/backend/models/Avatar.java
@@ -0,0 +1,48 @@
+package com._DMetaverse.backend.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="avatars")
+public class Avatar {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Long id;
+
+    @Column(name="image_url")
+    private String imageUrl;
+
+    @Column(name="name")
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
